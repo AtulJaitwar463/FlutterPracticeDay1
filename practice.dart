4590017325f15a884289019c5014fcc978069dc7
AtulJaitwar463 <144985694+AtulJaitwar463@users.noreply.github.com>
2023-10-08 02:13:45 +0530

    Add files via upload

---
+++ practice.dart
@@ -0,0 +1,76 @@
+void main() {
+  print('Welcome to dart');
+
+  // stdout.write("enter your name : ");
+  // var name = stdin.readLineSync();
+
+  // print('welcome,$name');
+
+  // new Human();
+
+  // //Declaration of variable
+
+  // int a;
+  // BigInt longValue;
+  // longValue = BigInt.parse('234560997765');
+
+  // num percentag = 45.7;
+  // bool islogin = false;
+  // //Assign
+  // a = 5;
+
+  // print(a);
+  // print(longValue);
+
+  String n = "Atul";
+  //var
+  // var section;
+  // section = 'D';
+  // section = 7;
+  // section = false;
+  // var rollmno = 11;
+  // rollmno = 16;
+
+//   var list = [10, 20, 30];
+//   list.add(50);
+
+//   var name = [];
+//   name.add("a");
+//   name.add("b");
+//   name.add("c");
+//   name.add("d");
+//   name[2] = 'atul'; //update nAME
+//   name.insert(2, 100);
+//   name.addAll(list);
+
+//   print("$list");
+
+//   list.replaceRange(0, 3, [1, 2, 3, 4]); //replace element
+
+// //remove elements from list
+//   list.removeLast();
+//   list.removeAt(1);
+//   print("$list");
+//   print("length : ${list.length}");
+//   print("Reversed : ${list.reversed}");
+//   print("first : ${list.first}");
+//   print("2nd index : ${list.elementAt(1)}");
+//print("otempty : ${list.isNotEmpty}");
+
+// //Map
+//   var map_name = {'name': 'value1', 'age': 2, 'value': 3.0};
+
+//   map_name['key1'] = "Atul";
+
+//   print(map_name);
+//   print("length : ${map_name.length}");
+//   print(" : ${map_name.containsValue(false)}");
+
+  final name = "raman";
+
+  final a = {"atul", "boy", "girl"};
+}
+
+class Human {
+  Human(); //default constructor
+}
